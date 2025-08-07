1bdd047b36017d704b5df34c379ff25b146b2137
senahan03 <ahansen092003@gmail.com>
2025-08-07 18:35:27 +0530

    opencv test

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import cv2
+
+vid_capture = cv2.VideoCapture('data/sample_videos/hungary2025.mp4')
+
+if (vid_capture.isOpened() == False):
+    print("Error opening the video file")
+else:
+    fps = int(vid_capture.get(5))
+    print("Frame Rate : ",fps,"frames per second") 
+    frame_count = vid_capture.get(7)
+    print("Frame count : ", frame_count)
+
