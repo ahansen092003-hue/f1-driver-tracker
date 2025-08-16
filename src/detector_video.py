cdb6734d7ae5c3929f0e845f4396b4d3b8e69361
senahan03 <ahansen092003@gmail.com>
2025-08-17 00:41:28 +0530

    refresh

--- src/detector_video.py
+++ src/detector_video.py
@@ -5,8 +5,13 @@ import time
 video_input = "data/sample_videos/hungary2025.mp4"
 video_save = "outputs/hungary2025_detected.mp4"
 
-def detect_video(video_path=video_input, save_path=video_save):
-    cap = cv2.VideoCapture(video_path)
+def detect_video(video_path, save_path):
+    vid_capture = cv2.VideoCapture(video_path)
+    if (vid_capture.isOpened() == False):
+        print("Error opening the video file")
+        return
+    
+    
 
 if __name__ == "__main__":
-    detect_video()
+    detect_video(video_input, video_save)
